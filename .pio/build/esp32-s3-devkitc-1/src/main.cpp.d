118812c122b1183dfd1d69c930d0964dc2d54b8f
vdupham <vdupham@gmail.com>
2025-10-04 23:47:29 +0700

    update TFT on lib folder

--- .pio/build/esp32-s3-devkitc-1/src/main.cpp.d
+++ .pio/build/esp32-s3-devkitc-1/src/main.cpp.d
@@ -178,15 +178,14 @@
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/cores/esp32/io_pin_remap.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/cores/esp32/Arduino.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/include/spi_flash/include/esp_spi_flash.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/TFT_eSPI.h \
+ lib/TFT_eSPI/TFT_eSPI.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/cores/esp32/Print.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/libraries/SPI/src/SPI.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/cores/esp32/esp32-hal-spi.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/User_Setup_Select.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/User_Setup.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/User_Setups/Setup203_ST7789.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/TFT_Drivers/ST7789_Defines.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Processors/TFT_eSPI_ESP32_S3.h \
+ lib/TFT_eSPI/User_Setup_Select.h lib/TFT_eSPI/User_Setup.h \
+ lib/TFT_eSPI/User_Setups/Setup203_ST7789.h \
+ lib/TFT_eSPI/TFT_Drivers/ST7789_Defines.h \
+ lib/TFT_eSPI/Processors/TFT_eSPI_ESP32_S3.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/include/soc/esp32s3/include/soc/spi_reg.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/include/driver/include/driver/spi_master.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/include/driver/include/driver/spi_common.h \
@@ -211,73 +210,66 @@
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/include/soc/esp32s3/include/soc/usb_serial_jtag_reg.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/libraries/FS/src/FS.h \
  /Users/phi/.platformio/packages/framework-arduinoespressif32@3.20014.231204/libraries/SPIFFS/src/SPIFFS.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/glcdfont.c \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font16.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font16.c \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font32rle.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font32rle.c \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font64rle.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font64rle.c \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font7srle.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font7srle.c \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font72rle.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Font72rle.c \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/gfxfont.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/TomThumb.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMono9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMono12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMono18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMono24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBold9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBold12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBold18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBold24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSans9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSans12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSans18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSans24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansOblique9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansOblique12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansOblique18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansOblique24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBold9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBold12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBold18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBold24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerif9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerif12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerif18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerif24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBold9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBold12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBold18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBold24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic9pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic12pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic18pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic24pt7b.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/User_Setups/User_Custom_Fonts.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Custom/Orbitron_Light_24.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Custom/Orbitron_Light_32.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Custom/Roboto_Thin_24.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Custom/Satisfy_24.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Fonts/Custom/Yellowtail_32.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Extensions/Smooth_font.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Extensions/Button.h \
- .pio/libdeps/esp32-s3-devkitc-1/TFT_eSPI/Extensions/Sprite.h
+ lib/TFT_eSPI/Fonts/glcdfont.c lib/TFT_eSPI/Fonts/Font16.h \
+ lib/TFT_eSPI/Fonts/Font16.c lib/TFT_eSPI/Fonts/Font32rle.h \
+ lib/TFT_eSPI/Fonts/Font32rle.c lib/TFT_eSPI/Fonts/Font64rle.h \
+ lib/TFT_eSPI/Fonts/Font64rle.c lib/TFT_eSPI/Fonts/Font7srle.h \
+ lib/TFT_eSPI/Fonts/Font7srle.c lib/TFT_eSPI/Fonts/Font72rle.h \
+ lib/TFT_eSPI/Fonts/Font72rle.c lib/TFT_eSPI/Fonts/GFXFF/gfxfont.h \
+ lib/TFT_eSPI/Fonts/GFXFF/TomThumb.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMono9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMono12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMono18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMono24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoOblique24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBold9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBold12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBold18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBold24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeMonoBoldOblique24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSans9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSans12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSans18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSans24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansOblique9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansOblique12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansOblique18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansOblique24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBold9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBold12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBold18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBold24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSansBoldOblique24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerif9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerif12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerif18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerif24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifItalic24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBold9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBold12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBold18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBold24pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic9pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic12pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic18pt7b.h \
+ lib/TFT_eSPI/Fonts/GFXFF/FreeSerifBoldItalic24pt7b.h \
+ lib/TFT_eSPI/User_Setups/User_Custom_Fonts.h \
+ lib/TFT_eSPI/Fonts/Custom/Orbitron_Light_24.h \
+ lib/TFT_eSPI/Fonts/Custom/Orbitron_Light_32.h \
+ lib/TFT_eSPI/Fonts/Custom/Roboto_Thin_24.h \
+ lib/TFT_eSPI/Fonts/Custom/Satisfy_24.h \
+ lib/TFT_eSPI/Fonts/Custom/Yellowtail_32.h \
+ lib/TFT_eSPI/Extensions/Smooth_font.h lib/TFT_eSPI/Extensions/Button.h \
+ lib/TFT_eSPI/Extensions/Sprite.h
